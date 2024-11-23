9aa7b1b5dc96c179cb9476987400ebe480a3e062
GithubIvanka <ivanivanka913@gmail.com>
2024-11-23 21:16:19 +0700

    23.11.2024

---
+++ .venv/module_1_6.py
@@ -0,0 +1,6 @@
+home_work_count = 12
+hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = hours_spent / home_work_count
+
+print("Курс: ", course_name, ", Всего задач: ", home_work_count, ", затрачено часов: ", hours_spent, ", среднее время выполнения ", time_for_one_task, sep='')
