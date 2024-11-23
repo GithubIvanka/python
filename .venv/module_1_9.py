20fedd23969d2d7468c37ae9d52f540e62a2571d
GithubIvanka <ivanivanka913@gmail.com>
2024-11-23 21:41:37 +0700

    23.11.2024

---
+++ .venv/module_1_9.py
@@ -0,0 +1,15 @@
+example = "Hello, world!"
+
+print(example[0])
+print(example[-1])
+print(example[7:])
+print(example[::-1])
+print(example[::2])
+
+example = 'Топинамбур'
+
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
