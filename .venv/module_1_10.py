4bbe9ddf0e4dd70da20698b08323d8c81e950df0
GithubIvanka <ivanivanka913@gmail.com>
2024-12-03 12:25:57 +0700

    3.12.2024

--- .venv/module_1_10.py
+++ .venv/module_1_10.py
@@ -33,3 +33,19 @@ dict_grades.update({sorted_students[3]: sum(grades[3]) / len(grades[3])})
 dict_grades.update({sorted_students[4]: sum(grades[4]) / len(grades[4])})
 
 print(dict_grades)
+
+#--------------------------------------------------------------------------
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students = sorted(students)
+
+my_dict = {
+    students[0]: sum(grades[0]) / len(grades[0]),
+    students[1]: sum(grades[1]) / len(grades[1]),
+    students[2]: sum(grades[2]) / len(grades[2]),
+    students[3]: sum(grades[3]) / len(grades[3]),
+    students[4]: sum(grades[4]) / len(grades[4])}
+
+print(my_dict)
