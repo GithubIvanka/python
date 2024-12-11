fca8f975ab4373dbe81c23bf04119e05031bcbd8
GithubIvanka <ivanivanka913@gmail.com>
2024-12-11 19:55:07 +0700

    11.12.2024

---
+++ module_3/module_3_1.py
@@ -0,0 +1,28 @@
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    str_len = len(string)
+    str_up = string.upper()
+    str_low = string.lower()
+    return str_len, str_up, str_low
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        i = i.lower()
+        string = string.lower()
+        if i == string:
+            return True
+    return False
+
+
+calls = 0
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
