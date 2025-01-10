79dc009e93053abcf0f50c9ecba87dd043a86f97
sibitti <ivanivanka913@gmail.com>
2025-01-10 21:13:08 +0700

    Дополнительное практическое задание по модулю: "Классы и объекты."

--- module_5/module_5_6.py
+++ module_5/module_5_6.py
@@ -1,5 +1,7 @@
+from time import sleep
+
 class UrTube:
-    users = []
+    users = {}
     videos = []
     current_user = None
 
@@ -11,7 +13,8 @@ class UrTube:
         if nickname in self.users:
             print(f"Пользователь {nickname} уже существует.")
         else:
-            self.users.append(User(nickname, password, age))
+            self.users[nickname] = User(nickname, password, age)
+            self.current_user = nickname
 
     def log_out(self):
         self.current_user = None
@@ -27,21 +30,79 @@ class UrTube:
         find_videos = []
         for video in self.videos:
             if str(find_string).lower() in str(video.title).lower():
-                find_videos.append(video)
+                find_videos.append(video.title)
         return find_videos
 
     def watch_video(self, title):
-        pass
+        if not self.current_user:
+            print("Войдите в аккаунт, чтобы смотреть видео")
+
+        elif self.users[self.current_user] < 18:
+            print("Вам нет 18 лет, пожалуйста покиньте страницу")
+        else:
+            for video in self.videos:
+                if video.title == title:
+                    for t in range(1, video.duration + 1):
+                        print(t, end=" ")
+                        sleep(1)
+                    print("Конец видео")
 
 class Video:
-    def __init__(self, title, duration, time_now, adult_mode=False):
+    def __init__(self, title, duration, time_now=0, adult_mode=False):
         self.title = title
         self.duration = duration
         self.time_now = time_now
         self.adult_mode = adult_mode
+
+    def __str__(self):
+        return self.title
 
 class User:
     def __init__(self, nickname, password, age):
         self.nickname = nickname
         self.password = hash(password)
         self.age = age
+
+    def __eq__(self, other):
+        return self.nickname == other
+
+    def __lt__(self, other):
+        return self.age < other
+
+    def __le__(self, other):
+        return self.age <= other
+
+    def __gt__(self, other):
+        return self.age > other
+
+    def __ge__(self, other):
+        return self.age >= other
+
+    def __str__(self):
+        return self.nickname
+
+if __name__ == "__main__":
+    ur = UrTube()
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+    # Добавление видео
+    ur.add(v1, v2)
+
+    # Проверка поиска
+    print(ur.get_videos('лучший'))
+    print(ur.get_videos('ПРОГ'))
+
+    # Проверка на вход пользователя и возрастное ограничение
+    ur.watch_video('Для чего девушкам парень программист?')
+    ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+    ur.watch_video('Для чего девушкам парень программист?')
+    ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+    ur.watch_video('Для чего девушкам парень программист?')
+
+    # Проверка входа в другой аккаунт
+    ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+    print(ur.current_user)
+
+    # Попытка воспроизведения несуществующего видео
+    ur.watch_video('Лучший язык программирования 2024 года!')
